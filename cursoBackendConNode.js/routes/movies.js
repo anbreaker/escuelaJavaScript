f2985cd8a0c05087f144ab7e81c82839432c4b2e
anbreaker <antunez19@gmail.com>
2020-09-23 13:33:53 +0200

    ejemplos simples de respuestas de las rutas no vigilado ni controlado...

--- cursoBackendConNode.js/routes/movies.js
+++ cursoBackendConNode.js/routes/movies.js
@@ -16,4 +16,59 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:movieId', async (req, res, next) => {
+  try {
+    const movies = await Promise.resolve(moviesMock[0]);
+
+    res.status(200).json({
+      data: movies,
+      message: 'Movie retrieved',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Created Movies
+router.post('/:movieId', async (req, res, next) => {
+  try {
+    const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+    res.status(201).json({
+      data: createdMovieId,
+      message: 'Movie Created',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update Movie
+router.put('/:movieId', async (req, res, next) => {
+  try {
+    const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+    res.status(200).json({
+      data: updatedMovieId,
+      message: 'Movie updated',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Deleted Movie
+router.put('/:movieId', async (req, res, next) => {
+  try {
+    const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+    res.status(200).json({
+      data: deletedMovieId,
+      message: 'Movie deleted',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
